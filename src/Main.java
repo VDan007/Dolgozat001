15dc0029e5c32f1aed3fe2ee9874b8b599fcf4e1
DanielVarjaskéri <varjaskerid@gmail.com>
2024-02-04 23:24:41 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
+/*
+ * File: Dolgozat001
+ * Author: Varjaskéri Dániel
+ * Copyright: 2024, Varjaskéri Dániel
+ * Group: Szoft II/1/E
+ * Date: 2024-02-04
+ * Github: https://github.com/VDan007/Dolgozat001
+ * Licenc: GNU GPL
+ */
 class Main{
 	
-	/*
-	* File: Dolgozat001
-	* Author: Varjaskéri Dániel
-	* Copyright: 2024, Varjaskéri Dániel
-	* Group: Szoft II/1/E
-	* Date: 2024-02-04
-	* Github: https://github.com/VDan007/Dolgozat001
-	* Licenc: GNU GPL
-	*/
 	
 	public static void main (String[] arg) {
 		
