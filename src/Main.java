8a0e579084686592ac312529b2b8ca5c22889d61
DanielVarjaskéri <varjaskerid@gmail.com>
2024-02-04 19:41:19 +0100

    done

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 class Main{
 	
+	/*
+	* File: Dolgozat001
+	* Author: Varjaskéri Dániel
+	* Copyright: 2024, Varjaskéri Dániel
+	* Group: Szoft II/1/E
+	* Date: 2024-02-04
+	* Github: https://github.com/VDan007/Dolgozat001
+	* Licenc: GNU GPL
+	*/
+	
 	public static void main (String[] arg) {
 		
-		System.out.println("Program started");
 		
 		Controller controller = new Controller();
 		
